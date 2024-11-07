6816381fffc10c3d4c308ad48a2ba4628705ff15
JeaMianh <JeaMianh@foxmail.com>
2024-11-07 19:17:33 +0800

    使用自定义链接脚本编译，删除元数据得到 bin 文件

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-5650157f85229eb7.d
@@ -0,0 +1,7 @@
+/home/jeamianh/Code/rcore/2024a-rcore-JeaMianh/os/target/riscv64gc-unknown-none-elf/release/deps/os-5650157f85229eb7: src/main.rs src/lang_items.rs src/entry.asm
+
+/home/jeamianh/Code/rcore/2024a-rcore-JeaMianh/os/target/riscv64gc-unknown-none-elf/release/deps/os-5650157f85229eb7.d: src/main.rs src/lang_items.rs src/entry.asm
+
+src/main.rs:
+src/lang_items.rs:
+src/entry.asm:
