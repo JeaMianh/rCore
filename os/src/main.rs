1dc25ceffe4c074f1e415b93d51bf55b3c707b67
JeaMianh <JeaMianh@foxmail.com>
2024-11-07 23:41:05 +0800

    分配函数栈，清空 bss

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,3 +6,27 @@ mod lang_items;
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
+#[no_mangle]
+fn rust_main() -> ! {
+    clear_bss();
+    loop {} 
+}
+
+fn clear_bss() {
+    extern "C" { // 访问外部符号
+        fn sbss(); // 指向 bss 段的起始地址
+        fn ebss(); // 指向 bss 段的结束地址
+    }
+
+    // 从 sbss 到 ebss 的内存区域全部清零
+    let start = sbss as usize;
+    let end = ebss as usize;
+
+    (start..end).for_each(|addr| {
+        unsafe {
+            (addr as *mut u8).write_volatile(0);
+        }
+    })
+
+}
+
